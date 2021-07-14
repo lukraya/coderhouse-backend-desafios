78428b9cc448435a423117a8365e2f97f3c01e04
Lucía Krayacich <luciakrayacich@gmail.com>
2021-07-14 10:51:50 -0300

    Router agregado.

--- servidor.js
+++ servidor.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const apiRouter = express.Router();
 
 let producto = require('./Productos');
   
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use('/api', apiRouter);
 
-app.get('/productos', (req, res)=>{
+//app.use('/static', express.static(__dirname + '/public'));
+
+//Example router
+/* apiRouter.get('/', (req, res)=>{
+    res.send("Get request received")
+}) */
+
+apiRouter.get('/productos', (req, res)=>{
     res.send(producto.listarProductos)
 });
 
-app.post('/productos', (req, res)=>{
+apiRouter.post('/productos', (req, res)=>{
     let toAdd = req.body;
     let prod = producto.nuevoProd(toAdd);
     res.send(prod)
 });
 
-app.get('/productos/:id', (req, res)=>{
+apiRouter.get('/productos/:id', (req, res)=>{
     let id = req.params.id;
     res.send(producto.filtrarId(id))
 })
