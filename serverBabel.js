218449a87204f9ee20aba7f0d2fe1fad82b13c14
Lucía Krayacich <luciakrayacich@gmail.com>
2021-08-08 04:55:40 -0300

    Desafío 14.

--- serverBabel.js
+++ serverBabel.js
@@ -1,17 +1,27 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
-var Server = require('socket.io').Server;
+
+var _require = require('socket.io'),
+    Server = _require.Server;
+
 var io = new Server(server);
 var dayjs = require('dayjs');
+
 var PORT = 8080;
+
 var productos = require('./Productos');
 var mensajes = [];
+
 app.use('/static', express.static('static'));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
 io.on('connection', function (socket) {
     socket.emit('mensajes', mensajes);
     socket.on('nuevo-mensaje', function (mensaje) {
@@ -25,11 +35,11 @@ io.on('connection', function (socket) {
         io.sockets.emit('enviar-producto', { productos: productos.listarProductos });
     });
 });
+
 server.listen(PORT, function (err) {
     if (err) {
         console.log(err);
-    }
-    else {
+    } else {
         console.log("Server listening on PORT", PORT);
     }
 });
